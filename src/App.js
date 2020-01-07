4a55ea5c98c9cdec87e6bb3a3499b354a680ea94
soumyajit4419 <ashish454570@gmail.com>
2020-01-07 23:49:49 +0530

    completed backend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import Input from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+import Cards from './Card'
+import { Grid } from '@material-ui/core';
+
+
 const axios = require('axios');
 
 class App extends Component {
 
   state = {
-    city: 'Bhubaneswar'
+    city: 'Bhubaneswar',
+    data: []
+
   }
 
   getCityName = (event) => {
@@ -15,26 +21,53 @@ class App extends Component {
       city: event.target.value
     })
   }
+
   handelSubmit = (event) => {
-    var x = this.state.city;
     event.preventDefault();
-    axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${x},in&appid=a54b92c2f23341e4db393f6badcf3d0c`)
-      .then(function (response) {
-        console.log(response);
+    let x = this.state.city;
+    axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${x},in&appid=a54b92c2f23341e4db393f6badcf3d0c&units=metric`)
+      .then((res) => {
+        this.setState({
+          data: res.data.list
+        })
+        console.log(this.state.data)
       })
-      .catch(function (error) {
-        console.log(error);
+      .catch((err) => {
+        console.log(err.message);
+
       });
+    ;
   }
+
   render() {
+    if (this.state.data.length > 0) {
+      let l = this.state.data.length
+      let blocks = [];
+      for (let i = 0; i < l; i++) {
+        blocks.push(<Cards className="card" key={i} date={this.state.data[i].dt_txt}
+          temp={this.state.data[i].main.temp}
+          maxTemp={this.state.data[i].main.temp_max}
+          minTemp={this.state.data[i].main.temp_min}
+          pressure={this.state.data[i].main.pressure}
+          humidity={this.state.data[i].main.humidity}
+          windSpeed={this.state.data[i].wind.speed}
+          description={this.state.data[i].weather[0].description} />)
+      }
+      return (
+        <Grid container spacing={4}>{blocks}</Grid>
+      )
+    }
+
     return (
 
       <div className="App" >
-        <form noValidate autoComplete="off" onSubmit={this.handelSubmit}>
-          <Input required className="city" id="cityName" label="Enter City Name" variant="outlined" size="small" color="primary" value={this.state.city} onChange={this.getCityName} />
+        <ValidatorForm autoComplete="off" onSubmit={this.handelSubmit}>
+          <TextValidator required type="text" className="city" id="cityName" label="Enter City Name" variant="outlined" size="small" color="primary" value={this.state.city} onChange={this.getCityName} />
           <br /><br />
-          <Button variant="outlined" color="primary" type="submit" >Search</Button>
-        </form>
+          <Button variant="outlined" color="primary" type="submit">Search</Button>
+        </ValidatorForm>
+        <br /><br /><br />
+
       </div>
     );
   }
